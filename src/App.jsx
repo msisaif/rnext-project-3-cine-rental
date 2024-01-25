73000d0da26281015f3f87398a32b9942a0a9cab
msisaif <msi132043@gmail.com>
2024-01-25 12:44:11 +0600

    cartMovies data change App to Page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,6 @@ import Page from "./Page";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
-  const [cartMovies, setCartMovies] = useState([]);
-
-  function handleAddToCart(movie) {
-    if (cartMovies.find((cartMovie) => cartMovie.id === movie.id)) {
-      return console.error(
-        `The movie ${movie.title} has been added to the cart already`
-      );
-    }
-
-    return setCartMovies([...cartMovies, movie]);
-  }
-
-  function handleRemoveFromCart(movie) {
-    setCartMovies(cartMovies.filter((cartMovie) => cartMovie.id !== movie.id));
-  }
 
   return (
     <div className={isDarkMode ? "dark" : ""}>
@@ -25,9 +10,6 @@ function App() {
         <Page
           isDarkMode={isDarkMode}
           onChangeMode={() => setIsDarkMode(!isDarkMode)}
-          cartMovies={cartMovies}
-          onAddToCart={handleAddToCart}
-          onRemoveFromCart={handleRemoveFromCart}
         />
       </div>
     </div>
--- src/Page.jsx
+++ src/Page.jsx
@@ -1,28 +1,39 @@
+import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import MovieGrid from "./cine/MovieGrid";
 import SideBar from "./cine/SideBar";
 
-export default function Page({
-  isDarkMode,
-  onChangeMode,
-  cartMovies,
-  onAddToCart,
-  onRemoveFromCart,
-}) {
+export default function Page({ isDarkMode, onChangeMode }) {
+  const [cartMovies, setCartMovies] = useState([]);
+
+  function handleAddToCart(movie) {
+    if (cartMovies.find((cartMovie) => cartMovie.id === movie.id)) {
+      return console.error(
+        `The movie ${movie.title} has been added to the cart already`
+      );
+    }
+
+    return setCartMovies([...cartMovies, movie]);
+  }
+
+  function handleRemoveFromCart(movie) {
+    setCartMovies(cartMovies.filter((cartMovie) => cartMovie.id !== movie.id));
+  }
+
   return (
     <>
       <Header
         isDarkMode={isDarkMode}
         onChangeMode={onChangeMode}
         cartMovies={cartMovies}
-        onRemoveFromCart={onRemoveFromCart}
+        onRemoveFromCart={handleRemoveFromCart}
       />
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <SideBar />
 
-          <MovieGrid onAddToCart={onAddToCart} />
+          <MovieGrid onAddToCart={handleAddToCart} />
         </div>
       </main>
       <Footer />
